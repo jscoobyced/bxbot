64a4ea4e38f4250bbeee4a336c22d554804bd67f
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-05-19 21:21:37 +0700

    Another attempt at script and SonarQube

--- runBuild.ps1
+++ runBuild.ps1
@@ -1,4 +1,3 @@
-Write-Host "ModuleName    : $env:ModuleName"
 Write-Host "Build version : $env:APPVEYOR_BUILD_VERSION"
 Write-Host "Author        : $env:APPVEYOR_REPO_COMMIT_AUTHOR"
 Write-Host "Branch        : $env:APPVEYOR_REPO_BRANCH"
@@ -11,15 +10,20 @@ dotnet restore
 
 choco install codecov
 
-choco install msbuild-sonarqube-runner
+$sonar = "C:\ProgramData\bxbot\sonar-msbuild"
+$source = "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.2.0.1214/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip"
+$destination = "C:\ProgramData\bxbot\sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip"
+Invoke-WebRequest $source -OutFile $destination
+7z x $destination -o$sonar
 
 if ( -Not $env:APPVEYOR_PULL_REQUEST_NUMBER )
 {
-    dotnet "C:\ProgramData\chocolatey\lib\msbuild-sonarqube-runner\tools\SonarScanner.MSBuild.dll" `
+    dotnet "$sonar\SonarScanner.MSBuild.dll" `
         begin `
         /k:"bxbot" `
         /d:sonar.organization="jscoobyced-github" `
         /d:sonar.host.url="https://sonarcloud.io" `
+        /d:sonar.cs.opencover.reportsPaths="coverage.xml"
         /d:sonar.login="$env:SonarKey" `
         /d:sonar.exclusions="coverage\**\*,**\*.xml,**\*.js"
 }
@@ -28,12 +32,12 @@ dotnet build
 
 if ( -Not $env:APPVEYOR_PULL_REQUEST_NUMBER )
 {
-    & dotnet C:\ProgramData\chocolatey\lib\msbuild-sonarqube-runner\tools\SonarScanner.MSBuild.dll `
+    dotnet "$sonar\SonarScanner.MSBuild.dll" `
         end `
         /d:sonar.login="$env:SonarKey" `
 }
 
-%USERPROFILE%\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe `
+$env:USERPROFILE\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe `
     -register:user `
     -oldStyle `
     -target:"c:\Program Files\dotnet\dotnet.exe" `
