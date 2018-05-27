6812621d9409a739a8b91f674735d6dd50ad68ba
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-05-27 23:21:27 +0700

    Improve TS code coverage

--- bxbot/ClientApp/__tests__/components/Home.spec.tsx
+++ bxbot/ClientApp/__tests__/components/Home.spec.tsx
@@ -2,60 +2,10 @@ import * as React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import { Home } from '../../components/Home';
 import { } from 'jest';
-import { RouteComponentProps } from 'react-router';
-import { match } from 'react-router-dom';
-import { UnregisterCallback, Href } from 'history';
-
-function getMockRouterProps<P>(data: P) {
-
-    let location = {
-        hash: "",
-        key: "",
-        pathname: "",
-        search: "",
-        state: {}
-    };
-
-    let props: RouteComponentProps<P> = {
-        match: {
-            isExact: true,
-            params: data,
-            path: "",
-            url: ""
-        },
-        location: location,
-        history: {
-            length: 2,
-            action: "POP",
-            location: location,
-            push: () => { },
-            replace: () => { },
-            go: (num) => { },
-            goBack: () => { },
-            goForward: () => { },
-            block: (t) => {
-                let temp: UnregisterCallback = () => { };
-                return temp;
-            },
-            createHref: (t) => {
-                var temp: Href = "";
-                return temp;
-            },
-            listen: (t) => {
-                let temp: UnregisterCallback = () => { };
-                return temp;
-            }
-
-        },
-        staticContext: {
-        }
-    };
-
-    return props;
-}
+import { MockRouterProps } from '../../MockRouterProps';
 
 describe('Home component', () => {
-    var routerProps = getMockRouterProps<{}>({});
+    var routerProps = MockRouterProps.getMockRouterProps<{}>({});
     it('should render without throwing an error', () => {
         expect(shallow(<Home match={routerProps.match} history={routerProps.history} location={routerProps.location} />)
             .exists())
---
+++ bxbot/ClientApp/__tests__/components/charts/Models.spec.ts
@@ -0,0 +1,26 @@
+import { } from 'jest';
+import { Pairing, FetchDataState } from '../../../components/charts/Models';
+
+describe('Charts models', () => {
+
+    it('can create and modify a Pairing instance', () => {
+        let pairing:Pairing = {
+            timestamp: 0,
+            close: 0,
+            high: 0,
+            open: 0,
+            low: 0
+        };
+        pairing.low = 5;
+        expect(pairing.low).toBe(5)
+    })
+
+    it('can create and modify a FetchDataState instance', () => {
+        let fetchDataState:FetchDataState = {
+            pairings: [],
+            loading: true
+        };
+        fetchDataState.loading = false;
+        expect(fetchDataState.loading).toBe(false)
+    })
+});
