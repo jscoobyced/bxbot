a34e264fedc0371681fb5e14dc043c457d0843a5
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-05-29 21:44:35 +0700

    Fix unit test sensitive to timezone (#16)

--- bxbot/ClientApp/__tests__/components/charts/DataFormatter.spec.ts
+++ bxbot/ClientApp/__tests__/components/charts/DataFormatter.spec.ts
@@ -1,6 +1,7 @@
 import { } from 'jest';
 import { DataFormatter } from '../../../components/charts/DataFormatter';
 import { Pairing } from '../../../components/charts/Models';
+import { DateUtil } from '../../../utils/DateUtils';
 
 describe('DataFormatter', () => {
 
@@ -29,6 +30,9 @@ describe('DataFormatter', () => {
         timestamp: -1
     };
 
+    const negativeHour = DateUtil.toHumanHours(new Date(-1));
+    const positiveHour = DateUtil.toHumanHours(new Date(0));
+
     it('format empty array', () => {
         const result = dataFormatter.formatBollingerData([], 0);
         expect(result).toEqual([]);
@@ -36,18 +40,21 @@ describe('DataFormatter', () => {
 
     it('format single value array', () => {
         let result = dataFormatter.formatBollingerData([zeroPairing], 0);
-        expect(result).toEqual([["07:00", 0, 0, 0, 0, undefined, undefined, undefined]]);
+        expect(result).toEqual([[positiveHour, 0, 0, 0, 0, undefined, undefined, undefined]]);
         result = dataFormatter.formatBollingerData([positivePairing], 0);
-        expect(result).toEqual([["07:00", 1, 1, 1, 1, undefined, undefined, undefined]]);
+        expect(result).toEqual([[positiveHour, 1, 1, 1, 1, undefined, undefined, undefined]]);
     })
 
     it('format negative array', () => {
         const result = dataFormatter.formatBollingerData([negativePairing], 0);
-        expect(result).toEqual([["06:59", -1, -1, -1, -1, undefined, undefined, undefined]]);
+        expect(result).toEqual([[negativeHour, -1, -1, -1, -1, undefined, undefined, undefined]]);
     })
 
     it('format array', () => {
         const result = dataFormatter.formatBollingerData([negativePairing, zeroPairing, positivePairing], 0);
-        expect(result).toEqual([["06:59", -1, -1, -1, -1, undefined, undefined, undefined], ["07:00", 0, 0, 0, 0, undefined, undefined, undefined], ["07:00", 1, 1, 1, 1, undefined, undefined, undefined]]);
+        expect(result).toEqual([
+            [negativeHour, -1, -1, -1, -1, undefined, undefined, undefined],
+            [positiveHour, 0, 0, 0, 0, undefined, undefined, undefined],
+            [positiveHour, 1, 1, 1, 1, undefined, undefined, undefined]]);
     })
 });
