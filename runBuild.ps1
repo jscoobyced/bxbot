949a2a6e3fb1904885bf8b74aaf29aaf356112b0
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-06-30 16:35:19 +0700

    Cleanup build script, document it.

--- runBuild.ps1
+++ runBuild.ps1
@@ -1,42 +1,49 @@
-Write-Host "Build version : $env:APPVEYOR_BUILD_VERSION"
-Write-Host "Author        : $env:APPVEYOR_REPO_COMMIT_AUTHOR"
-Write-Host "Branch        : $env:APPVEYOR_REPO_BRANCH"
-Write-Host "Repository    : $env:APPVEYOR_REPO_NAME"
-
-$pullrequest = $env:APPVEYOR_PULL_REQUEST_NUMBER
-$sonarkey = $env:SonarKey
-$version = $env:APPVEYOR_BUILD_VERSION
-$ghkey = $env:GitHubSonarKey
+# Setting up variables
+## Appveyor specific
+$pullRequestNumber = $env:APPVEYOR_PULL_REQUEST_NUMBER
 $repoName = $env:APPVEYOR_REPO_NAME
-$sonar = "$env:APPVEYOR_BUILD_FOLDER"
-$sonarbuild = "$sonar\sonar-scanner-msbuild"
+$buildFolder = "$env:APPVEYOR_BUILD_FOLDER"
+$repoBranch = $env:APPVEYOR_REPO_BRANCH
+$buildVersion = $env:APPVEYOR_BUILD_VERSION
+$sonarkey = $env:SonarKey
+$ghkey = $env:GitHubSonarKey
+$userDirectory = "$env:USERPROFILE"
+## Project specific names
+$organization = "jscoobyced-github"
+$sonarProjectKey = "bxbot"
 $sonarexclusion = "coverage\**\*,**\*.xml,**\*.js,tscoverage\**\*,**\jestsetup.ts,**\*.ico,**\Program.cs,**\Startup.cs,**\*.spec.ts,**\*.spec.tsx"
+$openCoverTarget = "bxbot-tests\bxbot-tests.csproj"
+$openCoverFilter = "+[bxbot]* +[bxbot-tests]* -[bxbot*]*Program -[bxbot*]*Startup"
+$clientAppPath = "bxbot"
+## When above is set, everything below is not dependent on any system
+$nugetPackagesFolder = "$userDirectory\.nuget\packages"
+$openCoverVersion = "4.6.519"
+$sonarBuild = "$buildFolder\sonar-scanner-msbuild"
 $source = "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.2.0.1214/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip"
-$destination = "$env:APPVEYOR_BUILD_FOLDER\sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip"
+$destination = "$buildFolder\sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip"
+
+# Installing pre-requisites
 Invoke-WebRequest $source -OutFile $destination
-7z x $destination -o"$sonarbuild"
-
-
-Set-Location bxbot
+7z x $destination -o"$sonarBuild"
+Set-Location $clientAppPath
 yarn install
 Set-Location ..
-
 dotnet restore
-
 choco install codecov
 
+# Preparing build
 $runSonar = FALSE
 
-if ( ("master" -Eq $env:APPVEYOR_REPO_BRANCH) -And ( -Not $env:APPVEYOR_PULL_REQUEST_NUMBER ) )
+if ( ("master" -Eq $repoBranch) -And ( -Not $pullRequestNumber ) )
 {
     Write-Host "Building for branch"
 
     $runSonar = 1
-    dotnet "$sonarbuild\SonarScanner.MSBuild.dll" `
+    dotnet "$sonarBuild\SonarScanner.MSBuild.dll" `
         begin `
-        /k:"bxbot" `
-        /v:"$version" `
-        /d:sonar.organization="jscoobyced-github" `
+        /k:"$sonarProjectKey" `
+        /v:"$buildVersion" `
+        /d:sonar.organization="$organization" `
         /d:sonar.host.url="https://sonarcloud.io" `
         /d:sonar.cs.opencover.reportsPaths="coverage.xml" `
         /d:sonar.typescript.lcov.reportPaths="tscoverage/lcov.info" `
@@ -44,15 +51,15 @@ if ( ("master" -Eq $env:APPVEYOR_REPO_BRANCH) -And ( -Not $env:APPVEYOR_PULL_REQ
         /d:sonar.exclusions="$sonarexclusion"
 }
 
-if ( $env:APPVEYOR_PULL_REQUEST_NUMBER )
+if ( $pullRequestNumber )
 {
     Write-Host "Building for Pull Request"
 
     $runSonar = 1
-    dotnet "$sonarbuild\SonarScanner.MSBuild.dll" `
+    dotnet "$sonarBuild\SonarScanner.MSBuild.dll" `
         begin `
-        /k:"bxbot" `
-        /d:sonar.organization="jscoobyced-github" `
+        /k:"$sonarProjectKey" `
+        /d:sonar.organization="$organization" `
         /d:sonar.host.url="https://sonarcloud.io" `
         /d:sonar.cs.opencover.reportsPaths="coverage.xml" `
         /d:sonar.login="$sonarkey" `
@@ -63,25 +70,27 @@ if ( $env:APPVEYOR_PULL_REQUEST_NUMBER )
         /d:sonar.github.oauth=$ghkey
 }
 
+# Building
 dotnet build
 
-& "$env:USERPROFILE\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe" `
+# Code Coverage and Analysis
+& "$nugetPackagesFolder\opencover\$openCoverVersion\tools\OpenCover.Console.exe" `
     -register:user `
     -oldStyle `
-    -target:"c:\Program Files\dotnet\dotnet.exe" `
-    -targetargs:"test --no-build bxbot-tests\bxbot-tests.csproj" `
-    -filter:"+[bxbot]* +[bxbot-tests]* -[bxbot*]*Program -[bxbot*]*Startup" `
+    -target:"dotnet.exe" `
+    -targetargs:"test --no-build $openCoverTarget" `
+    -filter:"$openCoverFilter" `
     -output:"coverage.xml"
 
 codecov -f coverage.xml
 
-Set-Location bxbot
+Set-Location $clientAppPath
 yarn test:coverage
 Set-Location ..
 
 if ( $runSonar )
 {
-    dotnet "$sonarbuild\SonarScanner.MSBuild.dll" `
+    dotnet "$sonarBuild\SonarScanner.MSBuild.dll" `
     end `
     /d:sonar.login="$sonarkey"
 }
