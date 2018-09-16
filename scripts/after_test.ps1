8c5d547987eecd57a65f5a8c313c4bb8807d163b
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-09-16 12:34:18 +0700

    Add debug

--- scripts/after_test.ps1
+++ scripts/after_test.ps1
@@ -5,6 +5,7 @@ dotnet publish --output=../../published
 Set-Location ../published
 New-Item -ItemType directory -Path ../publish
 7z a ../publish/bxbot.zip *
+Set-Location ../publish
+Get-ChildItem -Path '.'
 
 Write-Host -------------------------------------- Publish complete
-Get-ChildItem -Path '.'
