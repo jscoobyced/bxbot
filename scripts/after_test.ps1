bfa3708c85c19caed71f7e124220093a1619ce22
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-09-16 13:14:57 +0700

    Skip zip archive

--- scripts/after_test.ps1
+++ scripts/after_test.ps1
@@ -1,11 +1,6 @@
 Write-Host -------------------------------------- Publish
 
 Set-Location src
-dotnet publish --output=../../published
-Set-Location ../published
-New-Item -ItemType directory -Path ../publish
-7z a ../publish/bxbot.zip *
-Set-Location ../publish
-Get-ChildItem -Path '.'
+dotnet publish --output=../../publish
 
 Write-Host -------------------------------------- Publish complete
