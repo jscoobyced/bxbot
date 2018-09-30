7a36d3be5ed482b0f08df2ad4576d977a17e2c4e
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-09-30 16:00:47 +0700

    Feature/ts lint sq 5 (#28)
    * Try again single scanner pass by creating a dummy csproj

--- scripts/run_tests.ps1
+++ scripts/run_tests.ps1
@@ -1,49 +1,13 @@
 Write-Host -------------------------------------- Run test ------------------------------
 
-Write-Host -------------------------------------- ClientApp -----------------------------
+Write-Host --------------------------------- ClientApp Tests-----------------------------
 Set-Location $env:APPVEYOR_BUILD_FOLDER/$env:ClientApp
 yarn --silent run test:coverage
 codecov -f $env:TsCoverage
-
-if ( $env:APPVEYOR_PULL_REQUEST_NUMBER )
-{
-    & "$env:MsBuildScanner\$env:SonarScanner\sonar-scanner.bat" `
-    -D sonar.projectKey=$env:SonarProjectKey `
-    -D sonar.projectVersion=$env:APPVEYOR_BUILD_VERSION `
-    -D sonar.sources=. `
-    -D sonar.projectBaseDir=$env:APPVEYOR_BUILD_FOLDER/$env:ClientApp `
-    -D sonar.organization=$env:SonarOrg `
-    -D sonar.host.url=$env:SonarUrl `
-    -D sonar.typescript.lcov.reportPaths=$env:LcovInfo `
-    -D sonar.testExecutionReportPaths=$env:APPVEYOR_BUILD_FOLDER/$env:ClientApp/$env:TsReportPath `
-    -D sonar.typescript.tsconfigPath=$env:TsConfig `
-    -D sonar.login=$env:SonarKey `
-    -D sonar.exclusions="$env:SonarExclusions" `
-    -D sonar.analysis.mode=preview `
-    -D sonar.github.pullRequest=$env:APPVEYOR_PULL_REQUEST_NUMBER `
-    -D sonar.github.repository=$env:APPVEYOR_REPO_NAME `
-    -D sonar.github.oauth=$env:SonarGithubKey
-}
-elseif ( $env:APPVEYOR_REPO_BRANCH -Eq "master" )
-{
-    & "$env:MsBuildScanner\$env:SonarScanner\sonar-scanner.bat"  `
-    -D sonar.projectKey=$env:SonarProjectKey `
-    -D sonar.projectVersion=$env:APPVEYOR_BUILD_VERSION `
-    -D sonar.sources=. `
-    -D sonar.projectBaseDir=$env:APPVEYOR_BUILD_FOLDER/$env:ClientApp `
-    -D sonar.organization=$env:SonarOrg `
-    -D sonar.host.url=$env:SonarUrl `
-    -D sonar.typescript.lcov.reportPaths=$env:LcovInfo `
-    -D sonar.testExecutionReportPaths=$env:APPVEYOR_BUILD_FOLDER/$env:ClientApp/$env:TsReportPath `
-    -D sonar.typescript.tsconfigPath=$env:TsConfig `
-    -D sonar.login=$env:SonarKey `
-    -D sonar.exclusions="$env:SonarExclusions"
-}
-
 Set-Location $env:APPVEYOR_BUILD_FOLDER/$env:SrcPath
 
-Write-Host -------------------------------------- ClientApp done ------------------------
-Write-Host -------------------------------------- Netcore -------------------------------
+Write-Host -------------------------------- ClientApp Tests done ------------------------
+Write-Host -------------------------------- Netcore Tests -------------------------------
 
 & $env:USERPROFILE\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe `
 -register:user `
@@ -55,6 +19,8 @@ Write-Host -------------------------------------- Netcore ----------------------
 
 codecov -f $env:CsCoverage
 
+Write-Host -------------------------------------- SQ Analysis start ---------------------
+
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER )
 {
     dotnet "$env:MsBuildScanner\SonarScanner.MSBuild.dll" begin `
@@ -63,8 +29,11 @@ if ( $env:APPVEYOR_PULL_REQUEST_NUMBER )
     /d:sonar.projectBaseDir=$env:APPVEYOR_BUILD_FOLDER/$env:SrcPath `
     /d:sonar.organization=$env:SonarOrg `
     /d:sonar.host.url=$env:SonarUrl `
-    /d:sonar.cs.opencover.reportsPaths=$env:CsCoverage `
     /d:sonar.login=$env:SonarKey `
+    /d:sonar.cs.opencover.reportsPaths=$env:CsCoverage `
+    /d:sonar.typescript.lcov.reportPaths=$env:LcovInfo `
+    /d:sonar.testExecutionReportPaths=$env:APPVEYOR_BUILD_FOLDER/$env:ClientApp/$env:TsReportPath `
+    /d:sonar.typescript.tsconfigPath=$env:TsConfig `
     /d:sonar.exclusions="$env:SonarExclusions" `
     /d:sonar.analysis.mode=preview `
     /d:sonar.github.pullRequest=$env:APPVEYOR_PULL_REQUEST_NUMBER `
@@ -79,8 +48,11 @@ elseif ( $env:APPVEYOR_REPO_BRANCH -Eq "master" )
     /d:sonar.projectBaseDir=$env:APPVEYOR_BUILD_FOLDER/$env:SrcPath `
     /d:sonar.organization=$env:SonarOrg `
     /d:sonar.host.url=$env:SonarUrl `
-    /d:sonar.cs.opencover.reportsPaths=$env:CsCoverage `
     /d:sonar.login=$env:SonarKey `
+    /d:sonar.cs.opencover.reportsPaths=$env:CsCoverage `
+    /d:sonar.typescript.lcov.reportPaths=$env:LcovInfo `
+    /d:sonar.testExecutionReportPaths=$env:APPVEYOR_BUILD_FOLDER/$env:ClientApp/$env:TsReportPath `
+    /d:sonar.typescript.tsconfigPath=$env:TsConfig `
     /d:sonar.exclusions="$env:SonarExclusions"
 }
 
@@ -88,12 +60,10 @@ dotnet build
 
 if ( ( $env:APPVEYOR_REPO_BRANCH -Eq "master" ) -Or $env:APPVEYOR_PULL_REQUEST_NUMBER )
 {
-    Write-Host -------------------------------------- SQ Analysis start ---------------------
-
     dotnet "C:\ProgramData\chocolatey\lib\sonarscanner-msbuild-netcoreapp2.0\tools\SonarScanner.MSBuild.dll" end /d:sonar.login=$env:SonarKey
-
-    Write-Host -------------------------------------- SQ Analysis done- ---------------------
 }
+
+Write-Host -------------------------------------- SQ Analysis done- ---------------------
 
 Set-Location $env:APPVEYOR_BUILD_FOLDER
 
