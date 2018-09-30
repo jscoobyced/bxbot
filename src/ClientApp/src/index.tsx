7dc80092f166e21c38b2013bd7484933b88db0bb
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-09-30 16:37:29 +0700

    Fix more SQ errors

--- src/ClientApp/src/index.tsx
+++ src/ClientApp/src/index.tsx
@@ -8,16 +8,16 @@ const routes = RoutesModule.routes;
 
 class Index {
   public renderApp() {
-    if(document === null || typeof document === undefined) {
+    if(!document) {
       return;
     }
 
     const bases = document.getElementsByTagName('base');
-    if(bases === null || typeof bases === undefined || bases.length === 0) {
+    if(!bases || bases.length === 0 || !bases[0]) {
       return;
     }
     
-    const baseUrl = bases[0].getAttribute('href')!;
+    const baseUrl = bases[0].getAttribute('href');
     if(baseUrl === null || typeof baseUrl === undefined) {
       return;
     }
