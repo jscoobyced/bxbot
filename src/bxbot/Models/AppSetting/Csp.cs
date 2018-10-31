7305bc68b52e0460b189ba6d804430393558a84d
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2018-10-31 23:10:25 +0700

    Update to use appsettings to configure CSP (#36)

---
+++ src/bxbot/Models/AppSetting/Csp.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace bxbot
+{
+    public class Csp
+    {
+        public List<string> Styles { get; set; }
+
+        public List<string> Scripts { get; set; }
+
+        public List<string> Fonts { get; set; }
+
+        public string Report { get; set; }
+    }
+}
